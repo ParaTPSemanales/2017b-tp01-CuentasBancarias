5ce0262846cbdf3f06482d38077fb18c80b152a3
Juan Manuel Martin Mora <jmartinmora5@gmail.com>
2017-09-06 23:29:59 -0300

    VERDE: EL monto inicia en cero por defecto

--- src/cuentabancaria/Cuenta.java
+++ src/cuentabancaria/Cuenta.java
@@ -3,5 +3,26 @@ package cuentabancaria;
 public class Cuenta {
 
 	
+	public double mostrarSaldo()
+	{
+		return this.getSaldo();
+	}
+	
+	// Getters & Setters
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	private int numero;
+	private double saldo;
 	
 }
